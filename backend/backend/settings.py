25995104125004d800d49c01017694ce56f0c5d0
eliziebluiz <elizieb.l3@gmail.com>
2022-11-16 00:03:09 -0300

    fix: add finish endpoints;

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-a(g2qb(!on%1)q!o-c52^j=50d)4q$&)usth%!%px2fx*h(-yf'
+SECRET_KEY = 'django-insecure-a(g2qb(!on%1)q!o-c52^j=50d)4q$&)usth%!%px2fx*h(-yf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
