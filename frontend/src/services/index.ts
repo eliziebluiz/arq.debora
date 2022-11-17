65ce330318adf7f7157b77f3232e5c995e1f505f
eliziebluiz <elizieb.l3@gmail.com>
2022-11-17 02:09:27 -0300

    fix: add integration ngrok;

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/",
+  baseURL: "https://5dfd-2804-29b8-5006-5bb6-42e4-3d9e-c65e-a653.ngrok.io/",
   headers: {
     "Content-Type": "application/json",
   },
